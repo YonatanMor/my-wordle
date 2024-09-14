7ef18da36ab5dbd69f0625d0c18d5d90a3dcecc5
Yoni Mor <yoni304@gmail.com>
2024-09-14 13:11:28 +0300

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -53,19 +53,23 @@ export default function Index() {
     if (input.length === 5) {
       setInput("");
       setCurrentRow((prev) => prev + 1);
-      colorLetters();
+      if (currentRow === 5) {
+        alert("game over");
+      }
+      // colorLetters()
     }
   };
 
-  const colorLetters = () => {
-    const inputArr = input.split("");
-    inputArr.forEach((char, i) => {
-      if (char === targetWord[i]) {
-        console.log(1);
-      } else {
-        console.log(0);
-      }
-    });
+  const setBg = (char, i) => {
+    if (char === targetWord[i]) {
+      console.log("green");
+      return " bg-[#538D4E]";
+    } else if (targetWord.includes(char) && char !== "") {
+      console.log("yellow");
+      return "bg-[#B59F3B]";
+    }
+    console.log("dark");
+    return "bg-[#3A3A3C]";
   };
 
   const resetGame = async () => {
@@ -85,45 +89,50 @@ export default function Index() {
   };
 
   return (
-      <div>
-        <div className="flex justify-center">
-          <span className="text-3xl">{targetWord}</span>
-        </div>
-        <div className="grid h-[600px] w-full grid-cols-5 grid-rows-6 gap-2  bg-[#33a332] p-2">
-          {grid.map((row: String[][], rowIndex: number) =>
-            row.map((col: String[], colIndex: number) => (
-              <div
-                key={colIndex}
-                className="flex items-center justify-center rounded-lg bg-slate-300 text-[60px]"
-              >
-                {col}
-              </div>
-            ))
-          )}
-        </div>
-
-        <div className=" flex justify-around bg-slate-400 p-1">
-          <input
-            onKeyDown={(e) => e.key === "Enter" && submitRow()}
-            value={input}
-            onChange={updateBoard}
-            type="text"
-            maxLength={5}
-            className="rounded-md border-2 border-red-700 bg-slate-300"
-          />
-          <button
-            onClick={submitRow}
-            className="m-1 h-8 w-20 rounded-md border-2 border-black bg-red-400 px-[2px]"
-          >
-            Enter
-          </button>
-          <button
-            onClick={resetGame}
-            className="m-1 h-8 w-20 rounded-md border-2 border-black bg-red-400 px-[2px]"
-          >
-            Reset
-          </button>
-        </div>
+    <div>
+      <div className="flex justify-center">
+        <span className="text-3xl">{targetWord}</span>
       </div>
+      <div className="grid h-[600px]  grid-cols-5 grid-rows-6 gap-1 bg-black p-2">
+        {grid.map((row: String[], rowIndex: number) =>
+          row.map((col: String, colIndex: number) => (
+            <div
+              key={colIndex}
+              className={`w-18 h-18 flex items-center justify-center border text-[50px] text-white  ${
+                currentRow > rowIndex ? setBg(col, colIndex) : "bg-black"
+              }`}
+            >
+              {col.toUpperCase()}
+            </div>
+          ))
+        )}
+      </div>
+
+      <div className="flex justify-around bg-slate-400 p-1">
+        <input
+          onKeyDown={(e) => e.key === "Enter" && submitRow()}
+          value={input}
+          onChange={updateBoard}
+          type="text"
+          maxLength={5}
+          className="rounded-md border-2 border-red-700 bg-slate-300"
+        />
+        <button
+          onClick={submitRow}
+          className="m-1 h-8 w-20 rounded-md border-2 border-black bg-red-400 px-[2px]"
+        >
+          Enter
+        </button>
+        <button
+          onClick={resetGame}
+          className="m-1 h-8 w-20 rounded-md border-2 border-black bg-red-400 px-[2px]"
+        >
+          Reset
+        </button>
+      </div>
+      <h1 className="text-3xl">
+        limit keyboard inputs to letters (cancel space char)
+      </h1>
+    </div>
   );
 }
