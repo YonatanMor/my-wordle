8c87147887853f9a6bf30dc41e18916938e293a1
Yoni Mor <yoni304@gmail.com>
2024-09-16 18:16:18 +0300

    fix animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        rotate: "spin 0.5s linear forwards",
+      },
+      keyframes: {
+        spin: {
+          from: { transform: "rotateX(0deg)" },
+          to: { transform: "rotateX(180deg) scaleY(-1)" },
+        },
+      },
+    },
   },
   plugins: [],
 };
